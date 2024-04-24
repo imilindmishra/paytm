d9712a4d765d5c269a0d531b3cc7cd1341cad4d4
Milind <milindmishra11@gmail.com>
2024-04-25 04:16:15 +0530

    setting up backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const mainRouter = require("./routes/index.js")
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+//.use  is used to create middlewares or run middlewares before functions
+app.use("/api/v1", mainRouter);
+
+app.use("api/v2", v2Router);
+
+app.listen(3000);
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,1 +1,12 @@
-import express from 'express';
+const express = require('express');
+const userRouter = require('./user.js');
+
+const router = express.Router();
+
+app.use("/api/v1/user", userRouter);
+
+
+
+module.exports = router;
+
+
